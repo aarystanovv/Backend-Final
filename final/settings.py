bf111beebad7ea7f293225272b9b93bbe6b798aa
Tima <74903776+aarystanovv@users.noreply.github.com>
2023-05-13 19:10:54 +0600

    Update settings.py

--- final/settings.py
+++ final/settings.py
@@ -130,4 +130,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ttt.arystan@gmail.com'
-EMAIL_HOST_PASSWORD = 'bsguyjpkperjpwtx'
+EMAIL_HOST_PASSWORD = '___'
